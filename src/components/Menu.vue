318571d8650e7a1d02010fe1e6404bcdfe7f8f41
Rafael Saboia <r-leafar@hotmail.com>
2022-05-30 12:21:00 -0300

    Centralizado Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,37 +1,48 @@
 <template>
   <nav>
-    <button id="btn-pedido">Pedidos</button>
-    <button id="btn-historico">Históricos</button>
-    <button id="btn-apontamento">Apontamento</button>
+    <div class="container">
+      <button id="btn-pedido">Pedidos</button>
+      <button id="btn-historico">Históricos</button>
+      <button id="btn-apontamento">Apontamento</button>
+    </div>
   </nav>
 </template>
 <script>
 export default {
-    name:"Menu"
-}
+  name: "Menu",
+};
 </script>
 <style scoped>
-    nav>button{
-        width: 18%;
-        height: 100px;
-        border-radius: 50%;
-        border: 0px;
-        font-size: 36px;
-        color:white;
-        font-weight: bold;
-        margin-top: 3%;
-        margin-bottom: 3%;
-        }
-    #btn-pedido{
-        background-color: #64FB61;
-    }
-    #btn-historico{
-        background-color: #F54AEF;
-        margin-left: 5%;
-    }
+.container > button {
+  width: 18%;
+  height: 100px;
+  border-radius: 50%;
+  border: 0px;
+  font-size: 36px;
+  color: white;
+  font-weight: bold;
+  margin-top: 3%;
+  margin-bottom: 3%;
+}
+#btn-pedido {
+  background-color: #64fb61;
+}
+#btn-historico {
+  background-color: #f54aef;
+  margin-left: 5%;
+}
 
-    #btn-apontamento{
-        background-color: #FAE318;
-        margin-left: 5%;
-    }
+#btn-apontamento {
+  background-color: #fae318;
+  margin-left: 5%;
+}
+.container {
+  text-align: center;
+
+}
+@media only screen and (max-width: 844px) {
+  .container > button {
+    font-size: 20px;
+  }
+}
 </style>
