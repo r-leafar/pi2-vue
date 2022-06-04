32d98b40691a99e9584b852f02b92fbb942dc717
Rafael Saboia <r-leafar@hotmail.com>
2022-06-04 19:39:47 -0300

    adicionado text-shadow

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="container">
-    <div id="conteudo1"><div class="size-x pedidos">Pedidos</div></div>
-    <div id="conteudo2"><div class="size-x historicos">Históricos</div></div>
+    <div id="conteudo1"><div class="pedidos">Pedidos</div></div>
+    <div id="conteudo2"><div class="historicos">Históricos</div></div>
     <div id="conteudo3">
-      <div class="size-x apontamentos">Apontamentos</div>
+      <div class="apontamentos">Apontamentos</div>
     </div>
   </nav>
 </template>
@@ -13,81 +13,81 @@ export default {
 };
 </script>
 <style scoped>
-.container{
-    display :grid ;
-  
-    grid-template-columns: 1fr 1fr 1fr ;
-    grid-row: 2fr 1fr 1fr   ;
-    grid-template-areas: "titulo titulo titulo"
-                         "conteudo1 conteudo2 conteudo3" 
-                          "conteudo4 conteudo4 conteudo4";
-     position: relative; 
+.container {
+  display: grid;
+  text-shadow: 1px 1px 2px black;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row: 2fr 1fr 1fr;
+  grid-template-areas:
+    "conteudo1 conteudo2 conteudo3"
+    "conteudo4 conteudo4 conteudo4";
+  position: relative;
 }
-.pedidos{
-    width: 200px; 
-    height: 150px;
-    font-size: 30px;
-    text-transform: uppercase;
-    font-weight: bold;
-    color:white;
-    background-color: rgb(23, 206, 17);
-    border-radius: 50%;
-    margin: 0 auto 0 auto;
-    margin-block: 20px;
+.pedidos {
+  width: 200px;
+  height: 150px;
+  font-size: 30px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+  background-color: rgb(23, 206, 17);
+  border-radius: 50%;
+  margin: 0 auto 0 auto;
+  margin-block: 20px;
 
-    display: -webkit-flex;
-     display: -ms-flexbox;
-            display: flex;
-    -webkit-align-items: center;
-      -webkit-box-align: center;
-	 -ms-flex-align: center;
-	    align-items: center;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
   justify-content: center;
 }
-.historicos{
-    width: 200px; 
-    font-size: 30px;
-    text-transform: uppercase;
-    font-weight: bold;
-    color:white;
-    height: 150px;
-    background-color: rgb(240, 10, 201);
-    border-radius: 50%;
-    margin: 0 auto 0 auto;
-    margin-block: 20px;
-    display: -webkit-flex;
+.historicos {
+  width: 200px;
+  font-size: 30px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+  height: 150px;
+  background-color: rgb(240, 10, 201);
+  border-radius: 50%;
+  margin: 0 auto 0 auto;
+  margin-block: 20px;
+  display: -webkit-flex;
 
-     display: -ms-flexbox;
-            display: flex;
+  display: -ms-flexbox;
+  display: flex;
 
-    -webkit-align-items: center;
-      -webkit-box-align: center;
-	 -ms-flex-align: center;
-	    align-items: center;
-    
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+
   justify-content: center;
 }
-.apontamentos{
-    width: 200px; 
-    height: 150px;
-    font-size: 20px;
-    text-transform: uppercase;
-    font-weight: bold;
-    color:white;
-    background-color: rgb(247, 243, 10);
-    border-radius: 50%;
-    margin: 0 auto 0 auto;
-    margin-block: 20px;
-    display: -webkit-flex;
+.apontamentos {
+  width: 200px;
+  height: 150px;
+  font-size: 20px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+  background-color: rgb(247, 243, 10);
+  border-radius: 50%;
+  margin: 0 auto 0 auto;
+  margin-block: 20px;
+  display: -webkit-flex;
 
-     display: -ms-flexbox;
-            display: flex;
+  display: -ms-flexbox;
+  display: flex;
 
-    -webkit-align-items: center;
-      -webkit-box-align: center;
-	 -ms-flex-align: center;
-	    align-items: center;
-    
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+
   justify-content: center;
 }
 </style>
