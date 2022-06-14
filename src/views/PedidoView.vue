6abd681f66c4f4998c0cf7807e01f2b335baaca9
Rafael Saboia <r-leafar@hotmail.com>
2022-06-14 07:18:36 -0300

    Tranferido componente para o componente Pedido.vue

--- src/views/PedidoView.vue
+++ src/views/PedidoView.vue
@@ -1,17 +1,15 @@
 <template>
-  <Menu/>
  <Pedido/>
   <Footer/>
 </template>
 
 
 <script>
-import Menu from "../components/Menu.vue";
 import Footer from "../components/Footer.vue";
 import Pedido from "../components/Pedido.vue";
 export default {
   components:{
-    Menu,Footer,Pedido
+    Footer,Pedido
   }
 }
 </script>
