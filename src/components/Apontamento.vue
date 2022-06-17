0033d8d7da8dbad115b0e80da2ee41b4b592df2e
Rafael Saboia <r-leafar@hotmail.com>
2022-06-17 07:53:58 -0300

    Criado os layouts dos componentes

--- src/components/Apontamento.vue
+++ src/components/Apontamento.vue
@@ -1,6 +1,64 @@
 <template>
-  <Menu/>
+  <Menu />
   <main>
+    <div id="apontamento-secao-1">
+      <div
+        class="rTable"
+        style="width: 100%; margin-top: 15px; padding-left: 0px; padding-top: 0%"
+      >
+        <div class="rTableRow">
+          <div class="rTableHead">Pedido</div>
+          <div class="rTableHead">Trem</div>
+          <div class="rTableHead">Carro</div>
+          <div class="rTableHead">Descricao Falha</div>
+          <div class="rTableHead">Data</div>
+          <div class="rTableHead">Local</div>
+          <div class="rTableHead">P/T</div>
+          <div class="rTableHead">IN</div>
+          <div class="rTableHead">Status</div>
+        </div>
+        <div class="rTableRow">
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+        </div>
+      </div>
+    </div>
+    <div id="apontamento-secao-2">
+      <div class="rTable">
+        <div class="rTableRow">
+          <div class="rTableHead">Carro</div>
+          <div class="rTableHead">Equipamento</div>
+          <div class="rTableHead">Defeito</div>
+          <div class="rTableHead">Reparo</div>
+          <div class="rTableHead">Data</div>
+        </div>
+        <div class="rTableRow">
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+          <div class="rTableCell">Lorem ipsum</div>
+        </div>
+      </div>
+      <button id="btn-apontamento-atualizar" v-on:click="">Atualizar</button>
+    </div>
+    <div id="apontamento-secao-3">
+      <div class="rTable">
+        <div class="rTableRow">
+          <div class="rTableHead">Observação</div>
+        </div>
+        <div class="rTableRow">
+          <div class="rTableCell">Lorem ipsum</div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -16,8 +74,68 @@ export default {
 </script>
 
 <style scoped>
-main{
-    height:100vh;
-    background-color: rgb(247, 243, 10);
+#btn-apontamento-atualizar {
+  font-size: 25px;
+  text-shadow: 1px 1px 2px black;
+  font-family: sans;
+  border: 0px;
+  width: 150px;
+  border-radius: 100%;
+  height: 90px;
+  color: #fff;
+  background-color: #4784fb;
+  margin: 4% 6%;
+}
+main {
+  height: 100vh;
+  background-color: rgb(247, 243, 10);
+}
+#apontamento-secao-2 > div.rTable {
+  width: 75%;
+  padding-top: 5%;
+  float: left;
+}
+#apontamento-secao-3 > div.rTable {
+  width: 75%;
+  padding-top: 5%;
+  float: left;
+}
+/*Formatação da tabela*/
+.rTable {
+  display: table;
+  width: 100%;
+}
+.rTableRow {
+  display: table-row;
+}
+.rTableHeading {
+  display: table-header-group;
+  background-color: #c4c4c4;
+}
+.rTableHead {
+  background-color: #c4c4c4;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+.rTableCell,
+.rTableHead {
+  display: table-cell;
+  padding: 3px 10px;
+}
+.rTableCell {
+  background-color: white;
+}
+.rTableHeading {
+  display: table-header-group;
+  background-color: #c4c4c4;
+  font-weight: bold;
+}
+.rTableFoot {
+  display: table-footer-group;
+  font-weight: bold;
+  background-color: #ddd;
+}
+.rTableBody {
+  display: table-row-group;
 }
 </style>
--- src/components/Historico.vue
+++ src/components/Historico.vue
@@ -2,7 +2,7 @@
   <Menu />
   <main>
     <div id="secao1">
-      <div style="width: 15%;float:left;">
+      <div style="width: 15%; float: left">
         <div class="rTable" style="padding-top: 15px">
           <div class="rTableRow">
             <div class="rTableHead">Data Inicial</div>
@@ -21,7 +21,10 @@
         </div>
       </div>
       <div>
-        <div class="rTable" style="width: 25%; margin-top: 15px;padding-left:100px;padding-top:2%">
+        <div
+          class="rTable"
+          style="width: 25%; margin-top: 15px; padding-left: 100px; padding-top: 2%"
+        >
           <div class="rTableRow">
             <div class="rTableHead">Frota</div>
             <div class="rTableHead">Trem</div>
@@ -34,9 +37,39 @@
           </div>
         </div>
       </div>
+      <div id="secao1_3">
+        <button id="btn-historico-buscar" v-on:click="">Buscar</button>
+      </div>
     </div>
 
-    <div id="secao2"></div>
+    <div id="secao2">
+    <div
+          class="rTable"
+          style="width: 100%; margin-top: 15px; padding-left: 0px; padding-top: 3%"
+        >
+          <div class="rTableRow">
+            <div class="rTableHead">Usuario</div>
+            <div class="rTableHead">Trem</div>
+            <div class="rTableHead">Carro</div>
+            <div class="rTableHead">Equipamento</div>
+            <div class="rTableHead">Defeito</div>
+            <div class="rTableHead">Reparo</div>
+            <div class="rTableHead">Data</div>
+            <div class="rTableHead">Observação</div>
+          </div>
+          <div class="rTableRow">
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+            <div class="rTableCell">Lorem ipsum</div>
+          </div>
+        </div>
+    
+    </div>
   </main>
 </template>
 
@@ -52,10 +85,29 @@ export default {
 </script>
 
 <style scoped>
+#secao1_3 {
+  float: right;
+  margin-right: 25%;
+}
+#secao2{
+}
+#btn-historico-buscar {
+  font-size: 25px;
+  text-shadow: 1px 1px 2px black;
+  font-family: sans;
+  border: 0px;
+  width: 150px;
+  border-radius: 100%;
+  height: 90px;
+  color: #fff;
+  background-color: #4784fb;
+}
+
 main {
   height: 100vh;
   background-color: rgb(240, 10, 201);
 }
+/*Formatação da tabela*/
 .rTable {
   display: table;
   width: 100%;
